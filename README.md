2c3be67f82397a5e1a0bfa252569bf382e4c8b7d
ghostoflord <ghost2892003zzz@gmail.com>
2025-04-01 15:08:02 +0700

    create project

---
+++ README.md
@@ -0,0 +1,11 @@
+# Java Spring Starter template
+Template này được sử dụng cho series Java Spring Restful (dự án study with jhipter) của tác giả Ghost
+
+## Về tác giả
+Mọi thông tin về Tác giả Ghost, các bạn có thể tìm kiếm trên github
+## Version java:JDK 17
+## Extension run on VScode: extension pack Java and spring extension pack;
+
+
+
+
---
+++ src/main/java/com/jhipter/study/StudyApplication.java
@@ -0,0 +1,16 @@
+package com.jhipter.study;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+// @SpringBootApplication
+
+@SpringBootApplication(exclude = {
+		org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration.class })
+public class StudyApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(StudyApplication.class, args);
+	}
+
+}
